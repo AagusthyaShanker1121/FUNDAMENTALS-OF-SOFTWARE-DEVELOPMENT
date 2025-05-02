d13ac2ef9c1cc94e8664b3a3e0caa6b0ce7de291
samwp95 <sam_williams95@hotmail.com>
2025-05-02 18:26:38 +1000

    Removed tutorials folder.

--- tutorials/Tut 3 exc 3.py
+++
@@ -1,7 +0,0 @@
-import math
-
-radius = float(input("Enter circle radius: "))
-circle_area = math.pi * (radius ** 2)
-print(f"Circle area of radius: {circle_area:.3f}")
-volume = (4/3) * math.pi * (radius ** 3)
-print(f"Sphere volume of radius: {volume}")
--- tutorials/Tut 3 exc 7.py
+++
@@ -1,8 +0,0 @@
-import random
-n = int(input("Enter length of array: "))
-
-numbers = [0] * n
-for i in range(n):
-    numbers[i] = random.randint(0, 100)
-
-print(numbers)
