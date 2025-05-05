8b90b4e0c3d65c5f9045af6c5621143d6c3597e6
samwp95 <sam_williams95@hotmail.com>
2025-05-05 20:56:11 +1000

    Additional documentation added to users.

--- uniapp/test.py
+++ uniapp/test.py
@@ -1,4 +1,26 @@
+import random
 
-curr_list = {5: 'a', '67': 6}
-new_list = {}
-none_list = None
+
+
+# Generate a list of random courses, ids, grades, and marks
+courses = [
+    "Mathematics", "Physics", "Chemistry", "Biology", "History",
+    "Geography", "English", "Computer Science", "Economics", "Philosophy"
+]
+
+grades = ["A", "B", "C", "D", "E", "F"]
+
+data = {
+        "id": random.randint(1000, 9999),
+        "course": random.choice(courses),
+        "grade": random.choice(grades),
+        "marks": random.randint(0, 100)
+    }
+
+
+def print_to_table(d):
+    col_width = [x for x in d.keys()]
+    # col_width = [x for x in col_width if len(x) == max(len(x))]
+    print(col_width)
+
+print_to_table(data)
--- uniapp/users.py
+++ uniapp/users.py
@@ -1,5 +1,7 @@
 import re
 import pandas as pd
+from random import randint
+from utils import Enrolment
 
 class Person:
     def __init__(self, name, email, password):
@@ -85,8 +87,27 @@ class Student(Person):
             'password': self.password
         }
 
-    def enrol(self):
-        return "ENROL"
+    def enrol(self, courses, student_controller):
+        """
+        Enrols the student in a course. This method ensures that the student is not enrolled in more than 4 courses.
+        It randomly selects a course from the available courses and checks if the student is already enrolled in it.
+        If the course is not already enrolled, it creates a new enrolment and adds it to the student's enrolments list.
+        """
+        try:
+            if len(self.enrolments) == 4:
+                raise ValueError("A student can only be enrolled in up to 4 courses.")
+            else:
+                while self.enrolments is None or courses[course_idx] in [curr_enrol.course for curr_enrol in self.enrolments]:
+                    print(f"New Course: {courses[course_idx]}, Current Courses: {[curr_enrol.course for curr_enrol in self.enrolments]}")
+                    course_idx = randint(0, len(courses))
+                new_enrolment = Enrolment(
+                    student=self, 
+                    course=courses[course_idx], 
+                    semester=randint(1, 2), 
+                    student_controller=student_controller)
+                self.enrolments.append(new_enrolment)
+        except:
+            print(f"Error assigning enrolment.")            
     
     def unenrol(self):
         return "UNENROL"
